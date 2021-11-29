1c570ff745bed3c4ae8870609fe424ec77d08292
Dee <dgrowette@gmail.com>
2021-11-29 14:59:02 -0800

    added first container call from weather api

--- assets/app.js
+++ assets/app.js
@@ -1,10 +1,10 @@
-//API key - 24f9efd8440e18d85fdf4f69350c73a9
+
 
 //https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
 
 const timeEL = document.getElementById('time')
 const dateEl = document.getElementById('date')
-const currentWeatherCondition = document.getElementById('current-weather-condition')
+const currentWeatherConditionEl = document.getElementById('current-weather-condition')
 const timeZone = document.getElementById('time-zone')
 const countryEL = document.getElementById('country')
 const forecastEL = document.getElementById('weather-forecast')
@@ -12,6 +12,8 @@ const tempEL = document.getElementById('temp')
 
 const days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
 const months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec']
+
+const API_KEY = '24f9efd8440e18d85fdf4f69350c73a9'
 
 setInterval(() => {
     const time = new Date()
@@ -28,3 +30,39 @@ setInterval(() => {
     dateEl.innerHTML = days[day] + ', ' + date + ' ' + months[month]
 
 }, 1000)
+
+getWeatherData()
+function getWeatherData () {
+    navigator.geolocation.getCurrentPosition((success) => {
+
+        let {latitude, longitude } = success.coords
+
+        fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely&units=imperial&appid=${API_KEY}`)
+        .then(res => res.json())
+        .then(data => {
+            
+        console.log(data)
+        showWeatherData(data)
+        })
+    })
+}
+
+function showWeatherData (data){
+    let {humidity, wind_speed, uvi} = data.current
+
+    currentWeatherConditionEl.innerHTML = 
+    `<div class="weather-condition">
+        <div>Humidity</div>
+        <div>${humidity}%</div>
+    </div>
+    <div class="weather-condition">
+        <div>Wind Speed</div>
+        <div>${wind_speed} mph</div>
+    </div>
+    <div class="weather-condition">
+        <div>UV Index</div>
+        <div>${uvi}</div>
+    </div>`
+
+   
+}
