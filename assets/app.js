9ae4087eb4b5e4186dd32dbd91b84e9ff84b43d9
Dee <dgrowette@gmail.com>
2021-11-29 10:14:57 -0800

    added high and lows to js file

--- assets/app.js
+++ assets/app.js
@@ -4,7 +4,20 @@
 
 
 function pullInfo() {
-    const newName = document.getElementById("cityInput")
+    const newSearch = document.getElementById("cityInput")
     const cityName = document.getElementById("cityName")
-    cityName.innerHTML = "--" + newName.value + "--"
+    cityName.innerHTML = "--" + newSearch.value + "--"
 }
+
+fetch("https://api.openweathermap.org/data/2.5/forecast?q='+newSearch.value+'&appid=24f9efd8440e18d85fdf4f69350c73a9")
+.then(response => response.json())
+.then(data => {
+    for (i = 0; i < 5; i++) {
+        document.getElementById("day" + (i+1) + "Low").innerHTML = "Low:" + Number(data.list[i].main.temp_min -272.71)
+    }
+    for (i = 0; i < 5; i++) {
+        document.getElementById("day" + (i+1) + "High").innerHTML = "High:" + Number(data.list[i].main.temp_max -273.98)
+    }
+        
+    }
+)
